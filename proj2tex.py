28b5f2e3b9f997de705a8b0e02e7870142c4461a
Sasha Volokh <volokh@usc.edu>
2023-04-01 00:08:00 -0700

    progress on make_screenshots

--- proj2tex.py
+++ proj2tex.py
@@ -1,6 +1,10 @@
 import maya.cmds as cmds
 import maya.utils
 import maya.mel
+import maya.OpenMayaUI as OpenMayaUI
+import maya.OpenMaya as OpenMaya
+import os.path
+import math
 
 from typing import Optional
 
@@ -81,8 +85,100 @@ class Proj2Tex:
             cmds.shadingNode('lambert', name=proj.material(), asShader=True)
             cmds.connectAttr(proj.projection() + '.outColor', proj.material() + '.color')
 
+    @staticmethod
+    def _world_to_viewport_pt(view, pt):
+        p = OpenMaya.MPoint(pt[0], pt[1], pt[2])
+        util_x = OpenMaya.MScriptUtil()
+        ptr_x = util_x.asShortPtr()
+        util_y = OpenMaya.MScriptUtil()
+        ptr_y = util_y.asShortPtr()
+        unclipped = view.worldToView(p, ptr_x, ptr_y)
+        if unclipped:
+            x = util_x.getShort(ptr_x)
+            y = util_y.getShort(ptr_y)
+            return x, y, True
+        else:
+            return None, None, False
+
     def save_screenshots(self):
-        pass
+        xmin, ymin, zmin, xmax, ymax, zmax = cmds.exactWorldBoundingBox(self.target_mesh, calculateExactly=True)
+        bbox_pts = [
+            [xmin, ymin, zmin],
+            [xmin, ymin, zmax],
+            [xmin, ymax, zmin],
+            [xmin, ymax, zmax],
+            [xmax, ymin, zmin],
+            [xmax, ymin, zmax],
+            [xmax, ymax, zmin],
+            [xmax, ymax, zmax]
+        ]
+        scr_cam = cmds.camera(name='proj_screenshot_cam', orthographic=True)[0]
+        window = cmds.window('proj_screenshot_window')
+        form = cmds.formLayout()
+        meditor = cmds.modelEditor()
+        try:
+            cmds.formLayout(form, edit=True, attachForm=[
+                (meditor, 'top', 0),
+                (meditor, 'left', 0),
+                (meditor, 'bottom', 0),
+                (meditor, 'right', 0)
+            ])
+            cmds.showWindow(window)
+            cmds.window(window, edit=True, width=1280, height=720)
+            cmds.modelEditor(meditor, edit=True, camera=scr_cam, displayAppearance='wireframe',
+                             headsUpDisplay=False, handles=False, grid=False, manipulators=False, viewSelected=True)
+            view = OpenMayaUI.M3dView()
+            OpenMayaUI.M3dView.getM3dViewFromModelEditor(meditor, view)
+            viewWidth = view.portWidth()
+            viewHeight = view.portHeight()
+            for proj in self.projections:
+                cmds.setAttr(scr_cam + '.rotateX', cmds.getAttr(proj.place3dTexture() + '.rotateX'))
+                cmds.setAttr(scr_cam + '.rotateY', cmds.getAttr(proj.place3dTexture() + '.rotateY'))
+                cmds.setAttr(scr_cam + '.rotateZ', cmds.getAttr(proj.place3dTexture() + '.rotateZ'))
+
+                cmds.select(all=True)
+                cmds.modelEditor(meditor, edit=True, removeSelected=True)
+
+                cmds.select(self.target_mesh)
+                cmds.select(proj.place3dTexture(), add=True)
+                cmds.modelEditor(meditor, edit=True, addSelected=True)
+
+                cmds.select(self.target_mesh)
+                cmds.viewFit(scr_cam, fitFactor=0.95)
+
+                crop_xmin = float('inf')
+                crop_ymin = float('inf')
+                crop_xmax = -float('inf')
+                crop_ymax = -float('inf')
+                for pt in bbox_pts:
+                    x, y, unclipped = Proj2Tex._world_to_viewport_pt(view, pt)
+                    assert unclipped
+                    crop_xmin = min(crop_xmin, x)
+                    crop_ymin = min(crop_ymin, y)
+                    crop_xmax = max(crop_xmax, x)
+                    crop_ymax = max(crop_ymax, y)
+
+                tmp_image_path = proj.image_path + '.tmp' + os.path.splitext(proj.image_path)[1]
+                cmds.playblast(filename=tmp_image_path, editorPanelName=meditor, st=1, et=1, format='image',
+                               width=viewWidth, height=viewHeight, viewer=False)
+
+                print(proj.name, crop_xmin, crop_ymin, crop_xmax, crop_ymax)
+
+                screenshotWidth = view.playblastPortWidth()
+                screenshotHeight = view.playblastPortHeight()
+
+                ss_crop_xmin = int(crop_xmin/viewWidth*screenshotWidth)
+                ss_crop_ymin = screenshotHeight - int(crop_ymax/viewHeight*screenshotHeight)
+                ss_crop_xmax = int(crop_xmax/viewWidth*screenshotWidth)
+                ss_crop_ymax = screenshotHeight - int(crop_ymin/viewHeight*screenshotHeight)
+
+                # print('crop {}: {} {} {} {}'.format(proj.name, ss_crop_xmin, ss_crop_ymin, ss_crop_xmax, ss_crop_ymax))
+
+        finally:
+            cmds.deleteUI(meditor)
+            cmds.deleteUI(window)
+            cmds.delete(scr_cam)
+
 
     def make_layered_shader(self):
         pass
